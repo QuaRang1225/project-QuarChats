94b966883584cab31f45db6f411349ea58e3b7b9
QuaRang1225 <dbduddnd1225@gmail.com>
2024-02-07 17:43:34 +0900

    Utility 프레임워크 생성
    - Landing 프레림워크 스토리 보드추가
    - Landing 프레림워크 뷰 컨트롤러 추가

---
+++ QuarChats/Modules/Landing/Builder.swift
@@ -0,0 +1,15 @@
+//
+//  Builder.swift
+//  Landing
+//
+//  Created by 유영웅 on 2/7/24.
+//
+
+import Foundation
+import UIKit
+
+public final class Builder{
+    public static func build() -> UIViewController{
+        return UIViewController()
+    }
+}
---
+++ QuarChats/Modules/Landing/LandingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LandingViewController.swift
+//  Landing
+//
+//  Created by 유영웅 on 2/7/24.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
