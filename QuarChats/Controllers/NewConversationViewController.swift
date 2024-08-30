e6e815087dd03d044163c07206536a79ce0a8b1c
QuaRang <dbduddnd1225@gmail.com>
2024-08-30 15:28:38 +0900

    파일명 밍 위치 변경
    - 파일명 및 파일 구조 변경
    - 아이폰&아이패드 가로 모드 비활성화

---
+++ QuarChats/Controllers/NewConversationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewConversationViewController.swift
+//  QuarChats
+//
+//  Created by 유영웅 on 8/30/24.
+//
+
+import UIKit
+
+class NewConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
